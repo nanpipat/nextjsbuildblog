cb4c8759d51947631707ca612095443608c24b0d
nanpipat.k@gmail.com <nanpipat.k@gmail.com>
2020-11-09 09:37:27 +0700

    111

---
+++ ghost-cms-nextjs/pages/index.tsx
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import { getHeapCodeStatistics } from 'v8'
+import styles from '../styles/Home.module.scss'
+import Link from 'next/link'
+
+const {BLOG_URL,CONTENT_API_KEY} = process.env
+
+type Post = {
+  title: string
+  slug: string
+}
+
+async function getPosts() {
+  //curl "https://demo.ghost.io/ghost/api/v3/content/posts/?key=22444f78447824223cefc48062"
+  const res = await fetch(`${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt`
+  ).then((res) => res.json())
+
+  const posts = res.posts
+  return posts
+}
+
+export const getStaticProps = async ({ params }) => {
+  const posts = await getPosts()
+  return {
+    props: { posts }
+  }
+}
+
+
+const Home: React.FC<{ posts: Post[] }> = (props) => {
+
+  const { posts } = props
+  return (
+    <div className={styles.container}>
+      <h1>Hello to my Blog :)</h1>
+      <ul>
+        {posts.map((post, index) => {
+          return <li key={post.slug}>
+            <Link href="/post/[slug]" as={`/post/${post.slug}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Home
