0c586c4b45e3818c94cd2696a5ca0854ca774f83
nanpipat.k@gmail.com <nanpipat.k@gmail.com>
2020-11-23 10:14:08 +0700

    firt

--- ghost-cms-nextjs/pages/index.tsx
+++ ghost-cms-nextjs/pages/index.tsx
@@ -2,20 +2,97 @@ import Head from 'next/head'
 import { getHeapCodeStatistics } from 'v8'
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import moment from 'moment'
 
-const {BLOG_URL,CONTENT_API_KEY} = process.env
+const { BLOG_URL, CONTENT_API_KEY } = process.env
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: "#fff"
+  },
+  hero: {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://images.unsplash.com/photo-1558981852-426c6c22a060?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80')`,
+    height: "500px",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#fff",
+    fontSize: "4rem",
+    [theme.breakpoints.down("sm")]: {
+      height: 300,
+      fontSize: "3em"
+    }
+  },
+  blogsContainer: {
+    paddingTop: theme.spacing(3)
+  },
+  blogTitle: {
+    fontWeight: 800,
+    paddingBottom: theme.spacing(3)
+  },
+  card: {
+    maxWidth: "100%",
+  },
+  media: {
+    height: 240
+  },
+  cardActions: {
+    display: "flex",
+    margin: "0 10px",
+    justifyContent: "space-between"
+  },
+  author: {
+    display: "flex"
+  },
+  paginationContainer: {
+    display: "flex",
+    justifyContent: "center"
+  }
+}));
 
 type Post = {
   title: string
   slug: string
+  custom_excerpt: string
+  url: string
+  feature_image: string
+  published_at: string
+  primary_author: {
+    name: string
+    profile_image: string
+  }
+
 }
+
 
 async function getPosts() {
   //curl "https://demo.ghost.io/ghost/api/v3/content/posts/?key=22444f78447824223cefc48062"
-  const res = await fetch(`${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&fields=title,slug,custom_excerpt`
+  const res = await fetch(`${BLOG_URL}/ghost/api/v3/content/posts/?key=${CONTENT_API_KEY}&include=tags,authors`
   ).then((res) => res.json())
 
   const posts = res.posts
+  // console.log(posts, "dasdsadasd")
   return posts
 }
 
@@ -30,18 +107,80 @@ export const getStaticProps = async ({ params }) => {
 const Home: React.FC<{ posts: Post[] }> = (props) => {
 
   const { posts } = props
+  const classes = useStyles();
   return (
-    <div className={styles.container}>
-      <h1>Hello to my Blog :)</h1>
-      <ul>
-        {posts.map((post, index) => {
-          return <li key={post.slug}>
-            <Link href="/post/[slug]" as={`/post/${post.slug}`}>
-              <a>{post.title}</a>
-            </Link>
-          </li>
-        })}
-      </ul>
+    <div className="App">
+      <AppBar className={classes.appBar} position="static">
+        <Toolbar>
+          <Typography variant="h6" color="primary" >
+            Blog
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box className={classes.hero}>
+        <Box>Next js Blog</Box>
+      </Box>
+      <Container maxWidth="lg" className={classes.blogsContainer}>
+        <Typography variant="h4" className={classes.blogTitle} >
+          Articles
+          </Typography>
+        <Grid container spacing={3}>
+          {posts.map((post, index) => {
+            return <Grid item xs={12} sm={6} md={4}>
+              
+                <Card className={classes.card}>
+                <Link href="/post/[slug]" as={`/post/${post.slug}`}>
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      image={post.feature_image}
+                      title={post.title}
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {post.title}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                        {post.custom_excerpt}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  </Link>
+                  <CardActions className={classes.cardActions}>
+                    <Box className={classes.author}>
+                      <Avatar src={post.primary_author.profile_image} />
+                      <Box ml={2}>
+                        <Typography variant="subtitle2" component="p">
+                          {post.primary_author.name}
+                        </Typography>
+                        <Typography variant="subtitle2" color="textSecondary" component="p">
+                          {moment(post.published_at).format('LL')}
+                        </Typography>
+                      </Box>
+                    </Box>
+                    <Box>
+                      <BookmarkBorderIcon />
+                    </Box>
+                  </CardActions>
+                </Card>
+
+              
+
+            </Grid>
+          })}
+
+        </Grid>
+      </Container>
+      {/* <h1>Hello to my Blog :)</h1>
+        <ul>
+          {posts.map((post, index) => {
+            return <li key={post.slug}>
+              <Link href="/post/[slug]" as={`/post/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          })}
+        </ul> */}
     </div>
   )
 }
--- ghost-cms-nextjs/pages/post/[slug].tsx
+++ ghost-cms-nextjs/pages/post/[slug].tsx
@@ -1,15 +1,25 @@
 import Link from 'next/link'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import styles from '../../styles/Home.module.scss'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-const {BLOG_URL,CONTENT_API_KEY} = process.env
+const { BLOG_URL, CONTENT_API_KEY } = process.env
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        backgroundColor: "#fff"
+    }
+}))
 
 
 
-async function getPost(slug:string){
+async function getPost(slug: string) {
     const res = await fetch(`${BLOG_URL}/ghost/api/v3/content/posts/slug/${slug}?key=${CONTENT_API_KEY}&fields=title,slug,html`
     ).then((res) => res.json())
-  
+
     const posts = res.posts
     return posts[0]
 }
@@ -17,14 +27,14 @@ async function getPost(slug:string){
 export const getStaticProps = async ({ params }) => {
     const post = await getPost(params.slug)
     return {
-      props: { post }
+        props: { post }
     }
-  }
+}
 
 export const getStaticPaths = () => {
 
     return {
-        paths:[],
+        paths: [],
         fallback: true
     }
 }
@@ -33,29 +43,40 @@ type Post = {
     title: string
     slug: string
     html: string
-  }
+}
 
-const Post: React.FC<{post: Post}> = props => {
+const Post: React.FC<{ post: Post }> = props => {
     console.log(props)
 
-    const { post } = props 
+    const { post } = props
     const router = useRouter()
+    const classes = useStyles();
 
-    if(router.isFallback){
+    if (router.isFallback) {
         return <h1>Loading....</h1>
     }
 
     return (
-        <div className={styles.container}>
-           <p className={styles.goback}>
-				<Link href="/">
-					<a>Go back</a>
-				</Link>
-			</p>
-			<h1>{post.title}</h1>
-			<div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+        <>
+            <AppBar className={classes.appBar} position="static">
+                <Toolbar>
+                    <Typography variant="h6" color="primary" >
+                        Blog
+          </Typography>
+                </Toolbar>
+            </AppBar>
+            <div className={styles.container}>
+                <p className={styles.goback}>
+                    <Link href="/">
+                        <a>Go back</a>
+                    </Link>
+                </p>
+                <h1>{post.title}</h1>
+                <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
 
-        </div>
+            </div>
+        </>
+
     )
 }
 
